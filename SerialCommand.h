38d7df530e22ec0d1c0778c9b1923806e29c71aa
Kevin M <kevinmarx14@gmail.com>
2022-02-06 15:39:40 -0800

    Update files, convert defines to const ints, further implementation of fixed point

--- SerialCommand.h
+++ SerialCommand.h
@@ -15,15 +15,15 @@
   // Expression for determining if a character is hex (0 to F)
   #define IS_HEX(ch)((((int)ch >= 48) || ((int)ch <= 57)) || (((int)ch >= 65) || ((int)ch <= 70)))
 
-  #define COMMAND_SIZE  32   // Commands are 31 characters long (at most); we need to terminate with a null
-  #define DEBUG_DEFAULT 500  // We send a debug message every 500 ms by default
-  #define DELAY_COUNT   10   // The data can be late this many times before we give up on sending it
-  #define DELAY_TIME    50   // Wait this many microseconds for new data to appear on the serial buffer
-  #define REQ_CHAR      "p"  // Send this character for request. We choose a character that does not appear in any other communication
-  #define BAD_CMD_CHAR  "q"
-  #define OK_CMD_CHAR   "r"
+  #define COMMAND_SIZE 32   // Commands are 31 characters long (at most); we need to terminate with a null
+  constexpr int DEBUG_DEFAULT = 500;  // We send a debug message every 500 ms by default
+  constexpr int DELAY_COUNT   = 10;   // The data can be late this many times before we give up on sending it
+  constexpr int DELAY_TIME    = 50;   // Wait this many microseconds for new data to appear on the serial buffer
+  constexpr char REQ_CHAR     = 'p';  // Send this character for request. We choose a character that does not appear in any other communication
+  constexpr char BAD_CMD_CHAR = 'q';
+  constexpr char OK_CMD_CHAR  = 'r';
 
-  #define BAUDRATE 115200    // Set the baud rate
+  constexpr int BAUDRATE = 115200;    // Set the baud rate
 
   bool read_serial(char command[]);
   void send_ack(bool isValid);
